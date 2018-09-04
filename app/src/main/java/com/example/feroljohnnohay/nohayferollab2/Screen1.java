4419e17bd39f1c7037c31f2856ebf2c8213932c7
unknown <2015081392@ust-ics.mygbiz.com>
2018-09-04 23:06:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/feroljohnnohay/nohayferollab2/Screen1.java
@@ -0,0 +1,15 @@
+package com.example.feroljohnnohay.nohayferollab2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1);
+       // setContentView(R.layout.screen2);
+        //setContentView(R.layout.screen3);
+    }
+}
